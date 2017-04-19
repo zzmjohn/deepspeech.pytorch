9b0a708fbafa2844fbbca84a48f49d73f88875c3
SeanNaren <seannaren@hotmail.com>
2017-04-19 11:04:28 +0100

    Fixed arange

--- train.py
+++ train.py
@@ -95,7 +95,7 @@ def main():
         viz_windows = [None, None, None]
         loss_results, cer_results, wer_results = torch.Tensor(args.epochs), torch.Tensor(args.epochs), torch.Tensor(
             args.epochs)
-        epochs = torch.range(1, args.epochs)
+        epochs = torch.arange(1, args.epochs)
 
     try:
         os.makedirs(save_folder)
